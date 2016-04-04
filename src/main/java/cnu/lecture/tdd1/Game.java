e66da09fe22bd18e1b676cfcf5f8745af9443ff3
Acloy <sample3023@gmail.com>
2016-04-04 15:56:56 +0900

    test

--- src/main/java/cnu/lecture/tdd1/Game.java
+++ src/main/java/cnu/lecture/tdd1/Game.java
@@ -4,4 +4,14 @@ package cnu.lecture.tdd1;
  * Created by tchi on 2016. 4. 4..
  */
 public class Game {
+	private:
+		int score = 0;
+		
+	public:		
+		int score(){
+			return score;
+		}
+		void roll(int pin){
+			score+=pin
+		}
 }
--- src/test/java/cnu/lecture/tdd1/BowlingGameTest.java
+++ src/test/java/cnu/lecture/tdd1/BowlingGameTest.java
@@ -6,8 +6,29 @@ import org.junit.Test;
  * Created by tchi on 2016. 4. 4..
  */
 public class BowlingGameTest {
-    @Test
-    public void testGutterGame() {
-        Game g = new Game();
+	private Game g;
+    @Before
+    public void testCreateGame(){
+    	g = new Game();
     }
+    @Test
+    public void rollMany(int n,int pins){
+    	for(inti=0;i<n;i++) 
+    		g.roll(pins); 
+    }
+	@Test
+    public void testGutterGame() {        
+        int n = 20;
+        int pins = 0;
+        rollMany(n,pins);
+        assertEquals(0,g.score()); 
+    }
+    @Test
+    public void testAllOnes(){    	
+    	int n = 20;
+        int pins = 1;
+        rollMany(n,pins);
+    	assertEquals(20,g.score());     	
+    }
+   
 }
